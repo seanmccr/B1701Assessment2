1fef58195cc68d81fd8b7b86b432093474f71cef
seanmccr <seanmccrone1@gmail.com>
2023-11-08 22:46:45 +0000

    08.11.23 B1701 Update
    Beginning of file work

--- B1701Assessment2Analysis.R
+++ B1701Assessment2Analysis.R
@@ -1,3 +1,23 @@
 # ----- B1701 Assessment Two | 08.11.2023 -----
 
+# Loading in our dataset 
+
 WWC23Data <- read.csv("/Users/seanmccrone/Desktop/MASTERS DEGREE/Course Material/B1701/Week 3/R Studio Tutorial Work/ECData.csv")
+
+# Loading in libraries we may need
+
+library(ggplot2)
+library(devtools)
+library(tidyverse)
+library(R.utils)
+library(StatsBombR)
+library(tibble)
+
+
+shots_goals <- ECData %>%
+  group_by(team.name) %>% 
+  summarise(shots = sum(type.name=="Shot", na.rm = TRUE),
+          goals = sum(shot.outcome.name=="Goal", na.rm = TRUE)) 
+
+shots_goals$ConversionRate <- mutate("shots" / "goals")
+
